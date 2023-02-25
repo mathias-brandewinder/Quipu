7ce8cb9881966f128adf33ec50c95aaa665addb2
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2023-02-25 14:08:16 -0800

    Update check.fsx

--- src/Quipu/check.fsx
+++ src/Quipu/check.fsx
@@ -10,3 +10,11 @@ let solution_g = NelderMead.solve Configuration.defaultValue 0.01 (Objective.fro
 
 let h (x, y, z) = pown x 2 + pown y 2 + pown z 2
 let solution_h = NelderMead.solve Configuration.defaultValue 0.01 (Objective.from h) [| 100.0; 100.0; 100.0 |]
+
+type Test() =
+    member this.Instance(x: float) = pown x 2
+    static member Static(x: float) = pown x 2
+
+let test = Test()
+let solution_test = NelderMead.solve Configuration.defaultValue 0.01 (Objective.from test.Instance) [| 100.0 |]
+let solution_static = NelderMead.solve Configuration.defaultValue 0.01 (Objective.from Test.Static) [| 100.0 |]
