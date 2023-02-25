5327316d17a6751eb389afee805ef1e3aa256cf1
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2023-02-25 13:55:43 -0800

    Helper: rewrite a function as array function
    Convert functions of 1, 2, 3 floats into corresponding array-based version

--- src/Quipu/NelderMead.fs
+++ src/Quipu/NelderMead.fs
@@ -24,6 +24,23 @@ module NelderMead =
         abstract member Dimension: int
         abstract member Value: float [] -> float
 
+    type Objective () =
+        static member from (f: float -> float) =
+            { new IObjective with
+                member this.Dimension = 1
+                member this.Value x = f(x.[0])
+            }
+        static member from (f: (float * float) -> float) =
+            { new IObjective with
+                member this.Dimension = 2
+                member this.Value x = f (x.[0], x.[1])
+            }
+        static member from (f: (float * float * float) -> float) =
+            { new IObjective with
+                member this.Dimension = 3
+                member this.Value x = f (x.[0], x.[1], x.[2])
+            }
+
     let update (config: Configuration) (objective: IObjective) (simplex: (float []) []) =
 
         let dim = objective.Dimension
--- src/Quipu/check.fsx
+++ src/Quipu/check.fsx
@@ -2,10 +2,11 @@
 open Quipu
 open Quipu.NelderMead
 
-let objective = {
-    new IObjective with
-        member this.Dimension = 1
-        member this.Value x = pown x.[0] 2
-    }
+let f x = pown x 2
+let solution_f = NelderMead.solve Configuration.defaultValue 0.01 (Objective.from f) [| 100.0 |]
 
-let solution = NelderMead.solve Configuration.defaultValue 0.01 objective [| 100.0 |]
+let g (x, y) = pown x 2 + pown y 2
+let solution_g = NelderMead.solve Configuration.defaultValue 0.01 (Objective.from g) [| 100.0; 100.0 |]
+
+let h (x, y, z) = pown x 2 + pown y 2 + pown z 2
+let solution_h = NelderMead.solve Configuration.defaultValue 0.01 (Objective.from h) [| 100.0; 100.0; 100.0 |]
