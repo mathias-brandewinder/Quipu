4d0d4b558a7828b11e598b80ec3d6f972cf3b406
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2024-12-05 10:56:30 -0800

    Extract Objective to separate file

--- src/Quipu/NelderMead.fs
+++ src/Quipu/NelderMead.fs
@@ -44,32 +44,6 @@ type Configuration = {
         Termination = Termination.Configuration.defaultValue
         }
 
-type IObjective =
-    abstract member Dimension: int
-    abstract member Value: float [] -> float
-
-type Objective () =
-    static member from (f: float -> float) =
-        { new IObjective with
-            member this.Dimension = 1
-            member this.Value x = f(x.[0])
-        }
-    static member from (f: (float * float) -> float) =
-        { new IObjective with
-            member this.Dimension = 2
-            member this.Value x = f (x.[0], x.[1])
-        }
-    static member from (f: (float * float * float) -> float) =
-        { new IObjective with
-            member this.Dimension = 3
-            member this.Value x = f (x.[0], x.[1], x.[2])
-        }
-    static member from (dim: int, f: float[] -> float) =
-        { new IObjective with
-            member this.Dimension = dim
-            member this.Value x = f x
-        }
-
 exception UnboundedObjective
 exception AbnormalConditions of float [][]
 
