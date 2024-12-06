0a37e6a9ad6cab833f194784d137108bee9b59c6
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2024-12-06 14:33:38 -0800

    Extract Starting Point to separate file

---
+++ src/Quipu/StartingPoint.fs
@@ -0,0 +1,62 @@
+namespace Quipu
+
+type IStartingPoint =
+    abstract member create: int -> float[][]
+
+type StartingPoint =
+
+    static member zero =
+        { new IStartingPoint with
+            member this.create(dim: int): float array array =
+                Simplex.create dim
+                |> Simplex.vertices
+        }
+
+    static member fromValue (startingPoint: seq<float>) =
+        { new IStartingPoint with
+            member this.create (dim: int): float[][] =
+                let startingPoint = startingPoint |> Array.ofSeq
+                if startingPoint.Length <> dim
+                then failwith $"Invalid starting point dimension: {startingPoint.Length}, expected {dim}."
+                Simplex.create startingPoint
+                |> Simplex.vertices
+        }
+
+    static member fromValue (startingPoint: seq<float>, radius: float) =
+        { new IStartingPoint with
+            member this.create (dim: int): float[][] =
+                let startingPoint = startingPoint |> Array.ofSeq
+                if startingPoint.Length <> dim
+                then failwith $"Invalid starting point dimension: {startingPoint.Length}, expected {dim}."
+                Simplex.create (startingPoint, radius)
+                |> Simplex.vertices
+        }
+
+    static member fromValue (startingPoint: float) =
+        { new IStartingPoint with
+            member this.create (dim: int): float[][] =
+                let startingPoint = Array.singleton startingPoint
+                if startingPoint.Length <> dim
+                then failwith $"Invalid starting point dimension: {startingPoint.Length}, expected {dim}."
+                Simplex.create startingPoint
+                |> Simplex.vertices
+        }
+
+    static member fromValue (startingPoint: #seq<#seq<float>>) =
+        { new IStartingPoint with
+            member this.create (dim: int): float[][] =
+                let startingPoint =
+                    startingPoint
+                    |> Seq.map (Array.ofSeq)
+                    |> Array.ofSeq
+                    |> Array.distinct
+                startingPoint
+                |> Array.iter (fun point ->
+                    if point.Length <> dim
+                    then
+                        failwith $"Invalid starting point dimension: {point.Length}, expected {dim}."
+                    )
+                if startingPoint.Length <= dim
+                then failwith $"Invalid starting simplex size: {startingPoint.Length}, expected {dim + 1}."
+                startingPoint
+        }
