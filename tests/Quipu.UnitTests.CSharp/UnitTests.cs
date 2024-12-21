b972825083841d1aec83f6d8100cca87d898fe60
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2024-12-21 15:11:26 -0800

    C# demo test

--- tests/Quipu.UnitTests.CSharp/UnitTests.cs
+++ tests/Quipu.UnitTests.CSharp/UnitTests.cs
@@ -36,6 +36,31 @@ public class CSharpFluentInterface
     }
 
     [Fact]
+    public void Demo()
+    {
+        Func<Double,Double,Double> f =
+            (x, y) => Math.Pow(x - 1.0, 2) + Math.Pow(y - 2.0, 2);
+
+        var tolerance = 0.001;
+
+        var result =
+            NelderMead
+                .Objective(f)
+                .WithMaximumIterations(100)
+                .WithTolerance(tolerance)
+                .StartFrom(Start.Around(new double[] { 100.0, 100.0 }))
+                .Minimize();
+
+        Assert.True(result.HasSolution);
+        var solution = result.Solution;
+        Assert.Equal(Status.Optimal, solution.Status);
+
+        Assert.Equal(0.0, solution.Candidate.Value, tolerance);
+        Assert.Equal(1.0, solution.Candidate.Arguments[0], tolerance);
+        Assert.Equal(2.0, solution.Candidate.Arguments[1], tolerance);
+    }
+
+    [Fact]
     public void Test_One_Parameter()
     {
         var result =
