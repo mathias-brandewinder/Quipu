45dd57d16a823e4b58dc1f4398f46c88c48f8453
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2025-07-05 16:15:10 -0700

    Update Benchmarks.fs
    Add benchmark for safe vs unsafe versions

--- tests/Quipu.Benchmarks/Benchmarks.fs
+++ tests/Quipu.Benchmarks/Benchmarks.fs
@@ -16,6 +16,8 @@ type Benchmarks () =
         }
 
     [<Benchmark(Description="Beale function")>]
+    [<IterationCount(100)>]
+    [<InvocationCount(10000)>]
     member this.BealeFunction () =
 
         beale
@@ -24,11 +26,37 @@ type Benchmarks () =
         |> NelderMead.startFrom (Start.around [ 4.5; 4.5 ])
         |> NelderMead.solve
 
+    [<Benchmark(Description="Beale function, safe")>]
+    [<IterationCount(100)>]
+    [<InvocationCount(10000)>]
+    member this.BealeFunction_safe () =
+
+        beale
+        |> NelderMead.objective
+        |> NelderMead.safe
+        |> NelderMead.withConfiguration solverConfiguration
+        |> NelderMead.startFrom (Start.around [ 4.5; 4.5 ])
+        |> NelderMead.solve
+
     [<Benchmark(Description="Booth function")>]
+    [<IterationCount(100)>]
+    [<InvocationCount(10000)>]
     member this.BoothFunction () =
 
         booth
         |> NelderMead.objective
+        |> NelderMead.withConfiguration solverConfiguration
+        |> NelderMead.startFrom (Start.around [ 10.0; 10.0 ])
+        |> NelderMead.solve
+
+    [<Benchmark(Description="Booth function, safe")>]
+    [<IterationCount(100)>]
+    [<InvocationCount(10000)>]
+    member this.BoothFunction_safe () =
+
+        booth
+        |> NelderMead.objective
+        |> NelderMead.safe
         |> NelderMead.withConfiguration solverConfiguration
         |> NelderMead.startFrom (Start.around [ 10.0; 10.0 ])
         |> NelderMead.solve
